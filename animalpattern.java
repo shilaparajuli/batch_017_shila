ec9394413f6fe19c78006c05c735782295ca0bef
shilaparajuli <shila.parajuli@deerwalk.edu.np>
2015-01-26 13:38:15 +0545

    initial commit

---
+++ animalpattern.java
@@ -0,0 +1,14 @@
+/**
+ * Created by PC on 1/15/2015.
+ */
+public class animalpattern {
+    public static void main(String [] args) {
+        System.out.println("             '_'");
+        System.out.println("             (\u00A9 \u00A9) ");
+        System.out.println("  /===========\\/  ");
+        System.out.println(" /  ||  %%  ||");
+        System.out.println("*   ||------||");
+        System.out.println("    \u00A5 \u00A5    \u00A5 \u00A5");
+        System.out.println("    \"\"      \"\" ");
+    }
+}
